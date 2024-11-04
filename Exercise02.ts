0b5a4c79a193cfd51b921160fc04505c92ec0f4c
ChrolloLucilfer013 <gagazmujizatadam@gmail.com>
2024-11-05 00:07:04 +0700

    Updated

--- Exercise02.ts
+++ Exercise02.ts
@@ -3,11 +3,40 @@ const var01 = [25,2];
 
 for (let i = 0; i < 2; i++) {
     if (var01[i]%2){
-        console.log(var01[i]+" Even Value");
+        console.log(`${var01[i]} Even Value`); // for even value
     
     } else {
-        console.log(var01[i]+" Odd Value");
+        console.log(`${var01[i]} Odd Value`); // for odd value
     }
     
 }
 
+//Exercise No.2
+const var02 = [6,7];
+let primestatus:boolean = true;
+//Number = 1
+for (let index = 0; index < 2; index++) {
+    if (var02[index]==1){ 
+        console.log("1 is not prime number")
+    }
+    else if (var02[index]>1){
+        for (let y = 2; y <= var02[index]/2 ; y++) {
+            if (var02[index]%y == 0){
+                primestatus=false;
+
+                break;
+            } else {
+                primestatus=true;
+            }
+
+        }
+        
+    }
+if (primestatus){
+    console.log(`${var02[index]} is prime number`)
+} else {
+    console.log(`${var02[index]} is not prime number`)
+}
+
+    
+}
