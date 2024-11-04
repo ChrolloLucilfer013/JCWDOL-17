8561696e1597fcbeb05059b7d9eb24ed082c01c7
ChrolloLucilfer013 <gagazmujizatadam@gmail.com>
2024-11-04 23:29:58 +0700

    Update Exercise02.ts

---
+++ Exercise02.ts
@@ -0,0 +1,13 @@
+//Exercise N0.1
+const var01 = [25,2];
+
+for (let i = 0; i < 2; i++) {
+    if (var01[i]%2){
+        console.log(var01[i]+" Even Value");
+    
+    } else {
+        console.log(var01[i]+" Odd Value");
+    }
+    
+}
+
