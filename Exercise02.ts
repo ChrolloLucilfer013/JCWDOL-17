8be0b8c5d6675eebebc21dc90f21aab5096d64ce
ChrolloLucilfer013 <gagazmujizatadam@gmail.com>
2024-11-05 09:24:57 +0700

    Updated

--- Exercise02.ts
+++ Exercise02.ts
@@ -19,7 +19,7 @@ for (let index = 0; index < 2; index++) {
     if (var02[index]==1){ 
         console.log("1 is not prime number")
     }
-    else if (var02[index]>1){
+    else if (var02[index]>1){                           // Check Non prime number
         for (let y = 2; y <= var02[index]/2 ; y++) {
             if (var02[index]%y == 0){
                 primestatus=false;
@@ -47,9 +47,28 @@ let buff01 =[0,0];
 
 for (let index2 = 0; index2 < 2; index2++) {
     for (let index1 = 1; index1 <= var03[index2]; index1++) {
-        buff01[index2]=buff01[index2] + index1;
+        buff01[index2]=buff01[index2] + index1;         //sum value save on the buffer data
     }    
 }
 
 console.log(buff01[0]);
 console.log(buff01[1]);
+
+//Exercose No.4
+let var04 = [4,6]
+let Result04: number[] = [];
+let BuffResult:number = 0;
+
+for (let m = 0; m < 2; m++) {
+        for (let n = var04[m]-1; n >=1 ; n--) {
+            var04[m] = var04[m] * n ;
+              
+            }
+            Result04.push(var04[m]);  
+        }
+
+console.log(`Result is ${Result04[0]}`);
+console.log(`Result is ${Result04[1]}`);
+
+
+
