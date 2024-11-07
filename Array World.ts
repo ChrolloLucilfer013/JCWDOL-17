98bdc8ffff5fd88eb06d45f043f6b4bcfdedc8d4
ChrolloLucilfer013 <gagazmujizatadam@gmail.com>
2024-11-07 17:55:15 +0700

    Updatedd

--- Array World.ts
+++ Array World.ts
@@ -5,16 +5,16 @@ console.log(arr3[0]);
  //example new array dengan limit panjang array
  let arr_names:number[] = new Array(10)  
 
- for(var i = 0;i<arr_names.length;i++) { 
+ for(let i = 0;i<arr_names.length;i++) { 
     arr_names[i] = i % 2 
     console.log(`Nilai : ${arr_names[i]}`) 
  }
 
 //Concat
 let alpha:string[] = ["a", "b", "c"]; 
-let numvar:string[] = ["1","2","3"];
+let numlet:string[] = ["1","2","3"];
 
-let alphaNumeric = alpha.concat(numvar); 
+let alphaNumeric = alpha.concat(numlet); 
 console.log("alphaNumeric : " + alphaNumeric );
 
 //function for bult in array methode
@@ -40,7 +40,20 @@ alpha.forEach(function (value) {
 
 
 //indeof untuk mencari nilai index value di dalam sebuah array
-var indexoftest = [12, 5, 8, 130, 44].indexOf(130); //disini kita mencari nilai index dari nilai 130 didalam sebuah array
+let indexoftest = [12, 5, 8, 130, 44].indexOf(130); //disini kita mencari nilai index dari nilai 130 didalam sebuah array
 console.log("Index of 130 is : " + indexoftest );
 
+//join 
+let arrjoinstr = new Array("First","Second","Third"); 
+let arrjoinnum = new Array(1, 2, 3); 
 
+let strx = arrjoinstr.join(); 
+let strxx = arrjoinnum.join();
+
+console.log("Test Array.Join1 : " + strx + ","+ strxx);  
+          
+let stry = arrjoinstr.join(", "); 
+console.log("Test Array.Join2 : " + stry );  
+          
+let strz = arrjoinstr.join(" + "); 
+console.log("Test Array.Join3 : " + strz );
